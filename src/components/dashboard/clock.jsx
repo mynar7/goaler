d15a632f1a788d1319b8a2894940f148c1b656ee
Lee Warrick <leewarrick@gmail.com>
2019-01-01 14:46:19 -0500

    add weekday to clock

--- src/components/dashboard/clock.jsx
+++ src/components/dashboard/clock.jsx
@@ -5,8 +5,17 @@ class Clock extends Component {
     constructor(props) {
         super(props);
         this.d = new Date();
+        this.options = {
+            weekday: 'short',
+            year: 'numeric',
+            month: 'numeric',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric',
+        }
         this.state = {
-            time: this.d.toLocaleString()
+            time: this.d.toLocaleString('en-US', this.options)
         }
     }
 
@@ -21,7 +30,7 @@ class Clock extends Component {
     update = () => {
         if(this.unmounting) return;
         const d = new Date();
-        const newTime = d.toLocaleString();
+        const newTime = d.toLocaleString('en-US', this.options);
         if(newTime !== this.state.time) {
             this.setState({
                 time: newTime
