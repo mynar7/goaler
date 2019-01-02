d1cbe196d8c5e0596ead7acef00889155111f707
Lee Warrick <leewarrick@gmail.com>
2019-01-01 20:29:23 -0500

    update about

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Paper from '@material-ui/core/Paper';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid'
 import './about.css';
@@ -7,15 +8,27 @@ import './about.css';
 class About extends Component {
     render() {
         return (
-            <Grid container>
-                <Grid item>
-                    <Paper className="about">
-                        <Typography variant={"h3"}>
-                            Made by Lee
-                        </Typography>
-                    </Paper>
+            <div className="about">
+                <Grid container justify={"center"} alignItems={"center"}>
+                    <Grid item xs={12} lg={8}>
+                        <Card className="about-card">
+                            <CardContent>
+                                <Typography variant={"h4"} align={"center"}>
+                                    Thanks for using Goaler!
+                                </Typography>
+                                <br/>
+                                <Typography variant={"h5"} align={"center"}>
+                                    Contact me <a href="mailto:leewarrick@gmail.com" rel="noopener noreferrer" target="_BLANK">here</a>
+                                </Typography>
+                                <br/>
+                                <Typography variant={"h6"} align={"center"}>
+                                    © Lee Warrick 2019. All Rights Reserved.
+                                </Typography>
+                            </CardContent> 
+                        </Card>
+                    </Grid>
                 </Grid>
-            </Grid>
+            </div>
         )
     }
 }
