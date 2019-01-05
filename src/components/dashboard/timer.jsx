98deff28d94dfe5eb1a35c3d4e2e8644cdc099f2
lee-warrick <lee.warrick@capco.com>
2019-01-05 10:21:17 -0500

    add past due times

--- src/components/dashboard/timer.jsx
+++ src/components/dashboard/timer.jsx
@@ -17,7 +17,7 @@ class Timer extends React.Component {
         }
     }
     componentDidMount() {
-        if (this.props.date && this.state.counting) {
+        if (this.props.date /* && this.state.counting */) {
             this.updateTime();
         }
     }
@@ -31,19 +31,23 @@ class Timer extends React.Component {
     updateTime = () => {
         if (this.unMounting) return;
         // var future = Date.parse("December 31, 2019");
-        const timeRemaining = this.checkTime();
-        if (timeRemaining < 1000) {
-            return this.setState({
-                time: {
-                    days: "00",
-                    hours: "00",
-                    minutes: "00",
-                    seconds: "00"
-                },
-                counting: false,
-                // classes: "timer-strike_animate"
-            });
-        }
+        let timeRemaining = this.checkTime();
+        let counting;
+        timeRemaining < 0 ? counting = false : counting = true;
+        timeRemaining = Math.abs(timeRemaining);
+        // console.log(timeRemaining)
+        // if (timeRemaining < 1000) {
+        //     return this.setState({
+        //         time: {
+        //             days: "00",
+        //             hours: "00",
+        //             minutes: "00",
+        //             seconds: "00"
+        //         },
+        //         counting: false,
+        //         // classes: "timer-strike_animate"
+        //     });
+        // }
         let seconds = Math.floor(timeRemaining / 1000) % 60;
         let minutes = Math.floor((timeRemaining / 1000) / 60) % 60;
         let hours = Math.floor((timeRemaining / 1000) / 3600) % 24;
@@ -56,7 +60,7 @@ class Timer extends React.Component {
             time: {
                 days, hours, minutes, seconds
             },
-            counting: true
+            counting: counting
         });
         requestAnimationFrame(this.updateTime);
     }
@@ -71,12 +75,20 @@ class Timer extends React.Component {
                 {
                     this.state.counting
                     ? <span className={this.state.classes}>
-                        {`${this.state.time.days}:` +
+                        Due in:
+                        {` ${this.state.time.days}:` +
                             `${this.state.time.hours}:` +
                             `${this.state.time.minutes}:` +
                             `${this.state.time.seconds}`}
                     </span>
-                    : <span className="timer-strike">00:00:00:00</span>
+                    : <span className="timer-strike">
+                        Past Due:
+                        {` ${this.state.time.days}:` +
+                            `${this.state.time.hours}:` +
+                            `${this.state.time.minutes}:` +
+                            `${this.state.time.seconds}`}
+                    </span>
+                    // : <span className="timer-strike">00:00:00:00</span>
                 }
             </React.Fragment>
         )
